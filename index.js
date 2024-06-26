d4c4c6ff8cef21de246864190990977b413dc73d
Akshay Dinesh <akshay5632@gmail.com>
2024-06-26 16:56:20 +0530

    added new function to process audio files

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// import { parseFile } from "music-metadata";
+
 const express = require("express");
 const app = express();
 const fs = require("fs");
 const path = require("path");
+const musicMetadata = require("music-metadata");
+
+const parseFile = musicMetadata.parseFile;
 
 function startServer() {
   const port = process.env.PORT || 3000;
@@ -11,6 +16,37 @@ function startServer() {
 }
 
 const dirName = path.join(__dirname, "songs");
+
+async function processAudioFiles(mp3Files) {
+  const allMetaData = new Map();
+  for (file of mp3Files) {
+    const fileNameWithPath = path.join(dirName, file);
+    const metadata = await parseFile(fileNameWithPath, {
+      duration: true,
+      skipCovers: true,
+      skipPostHeaders: true,
+    });
+    const duration = metadata.format.duration;
+    allMetaData.set(`${file}`, duration);
+  }
+  return allMetaData;
+}
+
+app.post("/test", async (req, res) => {
+  const mp3Files = fs
+    .readdirSync(dirName)
+    .filter((file) => file.endsWith(".mp3"));
+
+  let i = 0;
+  let contentLength = 0;
+
+  mp3Files.forEach((file) => {
+    const fileName = path.join(dirName, file);
+    contentLength += fs.statSync(fileName).size;
+  });
+  const metaData = await processAudioFiles(mp3Files);
+  console.log("Meta data", metaData);
+});
 
 app.get("/stream", (req, res) => {
   const mp3Files = fs
